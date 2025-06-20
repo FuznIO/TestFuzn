9e31cd44b1814a80363583df66ec1f26baa858dc
Lars-Erik Kindblad <lars-erik.kindblad@outlook.com>
2025-06-20 07:40:27 +0200

    SampleApp

---
+++ src/SampleApp.WebApp/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using SampleApp.WebApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// EF Core + SQLite
+builder.Services.AddDbContext<ProductDbContext>(options =>
+    options.UseSqlite("Data Source=products.db"));
+
+// App Services
+builder.Services.AddTransient<ProductService>();
+
+var app = builder.Build();
+
+// Auto-create DB
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ProductDbContext>();
+    db.Database.EnsureCreated();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
