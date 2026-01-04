62aeb526725a997e50f16a3a3e5827f454e511c2
Lars-Erik Kindblad <lars-erik.kindblad@outlook.com>
2026-01-04 16:35:13 +0100

    .

---
+++ src/TestFuzn.Tests/ExecutionType/Load/MultipleScenariosTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Fuzn.TestFuzn.Tests.ExecutionType.Load;
+
+[TestClass]
+public class MultipleScenariosTests : Test
+{
+    [Test]
+    [Metadata("MetaKey1", "MetaValue2")]
+    [Metadata("MetaKey2", "MetaValue2")]
+    public async Task Test_multiple_scenarios()
+    {
+        var scenario1Executed = false;
+        var scenario2Executed = false;
+
+        var scenario2 = Scenario("Second scenario")
+            .Id("Scenario-2")
+            .Step("Step 1", (context) =>
+            {
+                scenario2Executed = true;
+            })
+            .Load().Simulations((context, simulations) => simulations.OneTimeLoad(30));
+
+        await Scenario("First scenario")
+            .Id("Scenario-1")
+            .Step("Step 1", (context) =>
+            {
+                scenario1Executed = true;
+            })
+            .Load().Simulations((context, simulations) => simulations.OneTimeLoad(20))
+            .Load().IncludeScenario(scenario2)
+            .Run();
+
+        Assert.IsTrue(scenario1Executed);
+        Assert.IsTrue(scenario2Executed);
+    }
+}
