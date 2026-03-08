0a268281e001a4ecccb41e121e0af05614f90910
Lars-Erik Kindblad <lars-erik.kindblad@outlook.com>
2026-03-08 19:30:11 +0100

    Add XML docs to MSTest integration

--- src/TestFuzn.Adapters.MSTest/TestFuznIntegration.cs
+++ src/TestFuzn.Adapters.MSTest/TestFuznIntegration.cs
@@ -2,8 +2,15 @@
 
 namespace Fuzn.TestFuzn;
 
+/// <summary>
+/// Provides MSTest integration entry points for initializing and cleaning up a TestFuzn test suite.
+/// </summary>
 public static class TestFuznIntegration
 {
+    /// <summary>
+    /// Initializes the TestFuzn test suite. Call from an <c>[AssemblyInitialize]</c> method.
+    /// </summary>
+    /// <param name="testContext">The MSTest <see cref="TestContext"/> for the current test run.</param>
     public static async Task Init(TestContext testContext)
     {
         var testFramework = new MsTestRunnerAdapter(testContext);
@@ -11,6 +18,10 @@ public static class TestFuznIntegration
         await TestFuznIntegrationCore.Init(testFramework);
     }
 
+    /// <summary>
+    /// Cleans up the TestFuzn test suite and writes final reports. Call from an <c>[AssemblyCleanup]</c> method.
+    /// </summary>
+    /// <param name="testContext">The MSTest <see cref="TestContext"/> for the current test run.</param>
     public static async Task Cleanup(TestContext testContext)
     {
         var testFramework = new MsTestRunnerAdapter(testContext);
